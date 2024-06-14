45698ec5f76d8b41e6999b9b0627248d4875a7ef
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-06-14 17:29:58 +0300

    String, Enum, StringBuilder

---
+++ 05_StringBuilder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Specialized;
+using System.Text;
+
+namespace _05_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello world";
+           
+            str += "world";
+            str += "world";
+            str += "world";
+            str += "world";
+            str += "world";
+            str += "world";
+            str += "world";
+            str += "world";
+            str += "world";
+
+            StringBuilder  builder = new StringBuilder();
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+            builder.Append("hello");
+            builder.Append("hello");
+            builder.Append("hello");
+            Console.WriteLine(builder);
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+            builder.AppendLine("dngdkhkd");
+            builder.Append("hello");
+            Console.WriteLine(builder);
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+            builder.Append("hello");
+            builder.Append("hello");
+            Console.WriteLine(builder);
+            Console.WriteLine($"Length : {builder.Length}");
+            Console.WriteLine($"Capacity : {builder.Capacity}");
+            
+
+
+
+        }
+    }
+}
