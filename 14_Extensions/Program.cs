2c51c26491c13dece6306323e1bd5c5ac8c0edc8
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-07-03 17:08:24 +0300

    Event, Delegates

---
+++ 14_Extensions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace _14_Extensions
+{
+    static class ExampleExtension
+    {
+        public static int NumberWords(this string data)
+        {
+            if( string.IsNullOrEmpty(data ))return 0;   
+            return data.Split(new char[] { ' ',',','.','?','!'}, StringSplitOptions.RemoveEmptyEntries).Length; 
+        }
+        public static int NumberSymbols(this string data, char s)
+        {
+            if (string.IsNullOrEmpty(data)) return 0;
+
+            int count = 0;
+            foreach (char c in data)
+            {
+                if(c == s) count++; 
+            }
+            return count;   
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Lorem Ipsum is simply dummy text of the printing and " +
+                "typesetting industry. Lorem Ipsum has been the industry's" +
+                " standard dummy text ever since the 1500s, when an unknown " +
+                "printer took a galley of type and scrambled it to make " +
+                "a type specimen book. ";
+            Console.WriteLine($"Count words : {str.NumberWords()}"); ;
+            Console.WriteLine($"Count letter of 'o' : {str.NumberSymbols('o')}"); ;
+            Console.WriteLine($"Count letter of 'b' : {str.NumberSymbols('b')}"); ;
+        }
+    }
+}
---
+++ 15_Event/Program.cs
@@ -0,0 +1,160 @@
+﻿namespace _15_Event
+{
+    public delegate void FinishAction();
+    public delegate void ExamDelegate(string t);
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public void PassExam(string task)
+        {
+            Console.WriteLine($"Student : {LastName}  {FirstName} pass exam {task}");
+        }
+    }
+    class Teacher
+    {
+        public event Action TestEvent;
+        //public event ExamDelegate ExamDelegate;  //auto event (auto propertty)
+        private ExamDelegate examDelegate;
+        public event ExamDelegate ExamDelegate
+        {
+            add 
+            {
+                examDelegate += value;
+                Console.WriteLine(value.Method.Name + "was added");
+            } 
+            remove 
+            {
+                examDelegate -= value;
+                Console.WriteLine(value.Method.Name + "was removed");
+            }  
+        }
+        public void CreateExam(string task)
+        {
+            //create exam 
+            //some code
+
+
+            ///call students
+            examDelegate?.Invoke(task);
+        }
+        public void TestAction()
+        {
+            TestEvent.Invoke();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Student> students = new List<Student>
+            {
+                new Student
+                {
+                     FirstName = "Olga",
+                     LastName = "Ivanchuk",
+                     Birthdate = new DateTime(2000, 5,7),
+                },
+                 new Student
+                {
+                     FirstName = "Bob",
+                     LastName = "Sincler",
+                     Birthdate = new DateTime(2002,12,17),
+                },
+                  new Student
+                {
+                     FirstName = "Elis",
+                     LastName = "Holms",
+                     Birthdate = new DateTime(2004, 7,12),
+                },
+                     new Student
+                {
+                     FirstName = "Petro",
+                     LastName = "Petruk",
+                     Birthdate = new DateTime(2004, 7,12),
+                }
+            };
+
+            Teacher teacher = new Teacher();
+            foreach (Student student in students)
+            {
+                teacher.ExamDelegate += new ExamDelegate(student.PassExam);
+            }
+            teacher.ExamDelegate -= students[3].PassExam;
+           
+           // teacher.ExamDelegate = null;
+            teacher.CreateExam("C# exam at 15 o'clock 16.08.2024 in 36 ayditory");
+
+            teacher.TestEvent += Console.Clear;
+            teacher.TestEvent -= Console.Clear;
+            teacher.TestEvent += delegate () { Console.ForegroundColor = ConsoleColor.Green; };
+            teacher.TestEvent += delegate () { Console.BackgroundColor = ConsoleColor.Yellow; };
+            teacher.TestEvent -= delegate () { Console.BackgroundColor = ConsoleColor.Yellow; };
+            teacher.TestEvent += () => Console.Beep(500, 1000);
+            teacher.TestEvent -= () => Console.Beep(500, 1000);
+            teacher.TestEvent += Teacher_TestEvent;
+            teacher.TestEvent -= Teacher_TestEvent;
+            teacher.TestAction();
+
+
+
+            //callback function
+            ////HardWork(Action1);//Very good
+            //HardWork(delegate () { Console.WriteLine("Very good"); });
+            //HardWork( ()=> Console.WriteLine("Very good" ));
+            //HardWork(Action2);//Good
+            //HardWork(Action3);//Soso
+            //HardWork(null);//Bad
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private static void Teacher_TestEvent()
+        {
+            Console.WriteLine("Auto-created method by pressing TAB");
+        }
+
+        static void HardWork(FinishAction action)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Operation {i + 1} working.....");
+                Thread.Sleep(random.Next(500));
+                Console.WriteLine($"Operation {i + 1} finished.....");
+            }
+            action?.Invoke();
+        }
+        //static void Action1()
+        //{
+        //    Console.WriteLine("Very good");
+        //}
+        static void Action2()
+        {
+            Console.WriteLine(" good");
+        }
+        static void Action3()
+        {
+            Console.WriteLine("So-so");
+        }
+
+    }
+}
